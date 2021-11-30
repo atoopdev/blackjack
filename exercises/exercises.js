96ab6ea47bcf15dd4f0dc94cba9c1240c95887b3
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-11-30 15:36:54 -0800

    Update exercises.js

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,11 +1,26 @@
-let age = 20;
+let age = 101;
 
 console.log("Age: ", age);
 
-if (age > 21) {
-    console.log("You are old enough to enter a Bar.")
-} else if (age === 21) {
-    console.log("You are exactly 21.")
+if (age < 100) {
+    console.log("Not elligible for birthday card!")
+} else if (age === 100) {
+    console.log("Here's your birthday card from the king!")
 } else {
-    console.log("You are under 21.")
+    console.log("You are over 100 - you already have a card!")
 }
+
+
+
+
+
+
+
+
+// console.log("Age: ", age);
+
+// if (age >= 21) {
+//     console.log("You are old enough to enter a bar.")
+// } else {
+//     console.log("You are under 21.")
+// }
