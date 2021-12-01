b02d069fcf9555cb0dc21c845e5fe4e17717174c
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-12-01 10:20:49 -0800

    added variable

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // go back and replace with random number generator
 let firstCard = 11;
-let secondCard = 9;
+let secondCard = 11;
 
 let sum = firstCard + secondCard;
 console.log("Contents of sum: ", sum);
@@ -12,20 +12,26 @@ let hasBlackjack = false;
 // tracks if game is still active
 let isAlive = true;
 
+// for communicating to player
+let message = "";
+
 //to win total must be exactly 21
 if (sum <= 20) {
     console.log("Contents of sum: ", sum);
-    console.log("Cards less than 21. Do you want to draw a new card?");
+    message="Cards less than 21. Do you want to draw a new card?";
+    console.log(message);
     console.log("IsAlive: ", isAlive)
 } else if (sum === 21) {
     hasBlackjack = true;
     isAlive = false;
     console.log("Contents of sum: ", sum);
-    console.log("Exactly 21! You've got blackjack!");
+    message = "Exactly 21! You've got blackjack!"
+    console.log(message);
     console.log("IsAlive: ", isAlive)
 } else {
     isAlive = false;
     console.log("Contents of sum: ", sum);
-    console.log("Over 21! Sorry, you are out of the game!");
+    message = "Over 21! Sorry, you are out of the game!";
+    console.log(message);
     console.log("IsAlive: ", isAlive)
 }
