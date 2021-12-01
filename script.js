d154fbcaa6893e03965ca6a1c80a8821942a8f80
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-12-01 14:28:23 -0800

    removed debugging console output

--- script.js
+++ script.js
@@ -26,24 +26,18 @@ sumEL.textContent = "Sum: " + sum;
     if (sum <= 20) {
         console.log("Contents of sum: ", sum);
         message="Cards less than 21. Do you want to draw a new card?";
-        console.log(message);
         messageEL.textContent = message;
         console.log("IsAlive: ", isAlive)
     } else if (sum === 21) {
         hasBlackjack = true;
-        isAlive = false;
-        console.log("Contents of sum: ", sum);
-        
+        isAlive = false; 
         message = "Exactly 21! You've got blackjack!"
         messageEL.textContent = message;
-        console.log(message);
         console.log("IsAlive: ", isAlive)
     } else {
         isAlive = false;
-        console.log("Contents of sum: ", sum);
         message = "Over 21! Sorry, you are out of the game!";
         messageEL.textContent = message;
-        console.log(message);
         console.log("IsAlive: ", isAlive)
     }
 }
