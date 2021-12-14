a93cc38a03c0eccf1762979363c4c81a5eb5dc49
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-12-13 16:29:41 -0800

    last exercises

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,6 +1,14 @@
+let fruit = ["apple", "orange", "apple", "apple", "orange"];
+let appleShelf = document.getElementById("apple-shelf");
+let orangeShelf = document.getElementById("orange-shelf");
 
-
-
+for (let i=0; i<fruit.length; i++){
+    if(fruit[i]==="apple"){
+        appleShelf.textContent+= fruit[i] + " ";
+    } else if (fruit[i] ==="orange"){
+        orangeShelf.textContent+= fruit[i] + " ";
+    }
+}
 
 // return random item from array
 // let hands = ["rock", "paper", "scissor"];
